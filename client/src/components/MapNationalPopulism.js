fbf37ba7a40ac325fff88c519f60724bdad7b35e
izaakrogan <izaakrogan@googlemail.com>
2020-02-19 14:15:05 +0000

    [feature/wip] tool tip

--- client/src/components/MapNationalPopulism.js
+++ client/src/components/MapNationalPopulism.js
@@ -1,31 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import * as d3 from 'd3';
 
 import data from '../assets/mapEuropeWithElectionData';
 
 export default function NationalPopulism() {
-  let [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0)
+  const ref = useRef(0);
 
   useEffect(() => {
     mountD3Map();
   });
 
   useEffect(() => {
-    if (index >= 39) return;
+    if (ref.current >= 39) return;
     let id = setTimeout(() => {
-      setIndex(index + 1);
+      ref.current = ref.current + 1;
+      setIndex(index+1)
     }, 300);
     return () => clearTimeout(id);
   });
 
   useEffect(() => {
-    reColourMap(index);
-  }, [index]);
+    reColourMap(ref.current);
+  }, [ref.current]);
 
-  function reColourMap(index) {
+  useEffect(()=> {
+    updateToolTip()
+  }, [index])
+
+  function reColourMap(refCurrent) {
     d3.selectAll('path').attr('fill', country => {
-      const value = country.properties.electionData?.[index].value;
+      const value = country.properties.electionData?.[refCurrent].value;
       const noDataForCountry = !value;
       const noDataForYear = value === 'x';
       const transparent = noDataForCountry || noDataForYear;
@@ -41,10 +47,11 @@ export default function NationalPopulism() {
       .scale(1500 / Math.PI / 2)
       .translate([580, 450]);
 
-    d3.select('#map')
+    const map = d3.select('#map')
       .attr('width', 2000)
       .attr('height', 1000)
-      .selectAll('.country-groups')
+
+    const countryGroups = map.selectAll('.country-groups')
       .data(data.features)
       .enter()
       .append('g')
@@ -55,6 +62,46 @@ export default function NationalPopulism() {
       .attr('opacity', 0.5)
       .attr('stroke', 'lightsteelblue')
       .attr('transform', `scale(2) translate(-300, 0)`);
+
+    const tooltipPadding = 10;
+    const tooltipGroup = countryGroups.append("g").attr("class", "tooltip");
+    const tooltipRect = tooltipGroup.append("rect");
+    const tooltipYearsText = tooltipGroup
+      .append("text")
+      .attr("class", "years-text");
+    const tooltipCountryText = tooltipGroup
+      .append("text")
+      .attr("class", "country-text");
+      countryGroups.on("mouseover", d => {
+      tooltipGroup.style("visibility", "visible");
+    })
+    .on("mousemove", d => {
+
+      tooltipGroup.attr(
+        "transform",
+        `translate(${d3.event.offsetX + tooltipPadding},${d3.event.offsetY})`
+      );
+        tooltipRect
+          .attr("width", 200)
+          .attr("height", 60)
+          .attr("fill", "white");
+    })
+    // .on("mouseout", () => tooltipGroup.style("visibility", "hidden"));
+  }
+
+  function updateToolTip() {
+
+    // d3.selectAll('.country-groups').attr('width', d=> console.log('dddd', d))
+    d3.selectAll('.country-text')
+          .text(d=> {   
+            if(!d.properties.electionData) return
+           return d.properties.electionData[ref.current].value})
+          .style("fill", "red")
+          .style("z-index", "100")
+          .style("font-size", "10px")
+          .attr("dx", "5")
+          .attr("dy", "13");
+    
   }
 
   return (
